acb2bb86429ce916eed1bad6f66937b6b7437c1f
Peter Neave <peter.neave@purple.telstra.com>
2023-11-10 15:12:30 +1100

    Move script into separate file

---
+++ generateawsproducts.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Define the URL
+url='https://aws.amazon.com/api/dirs/items/search?item.directoryId=aws-products&sort_by=item.additionalFields.productCategory&sort_order=asc&size=500&item.locale=en_US&tags.id=!aws-products%23type%23feature&tags.id=!aws-products%23type%23variant'
+
+# Use curl to fetch data from the URL and store it in a variable
+data=$(curl -s "$url")
+
+# Check if curl was successful
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to fetch data from the URL."
+  exit 1
+fi
+
+markdown_file="awsproducts.md"
+markdown_content="<!--
+marp: true
+title: AWS Products
+theme: default
+class:
+- invert
+paginate: true
+-->
+
+<!-- paginate: skip -->
+# AWS Products
+Generated from https://aws.amazon.com/products
+
+[![w:48](img/github-mark-white.svg)](https://github.com/peterneave/awsproducts)
+<!-- footer: Last updated $(date) -->
+
+---
+<!-- paginate: true -->
+<!-- footer: '' -->
+"
+echo -e "$markdown_content" > "$markdown_file"
+
+# TOC
+markdown_content="
+<!-- header: '' -->
+
+## Product Categories
+"
+echo -e "$markdown_content" >> "$markdown_file"
+
+markdown_content=$(echo $data | \
+  jq -r '.items | group_by(.item.additionalFields.productCategory)[] | .[0] | "[\(.item.additionalFields.productCategory)](#\(.item.additionalFields.productNameLowercase | gsub(" ";"-") | gsub("\\(";"") | gsub("\\)";"") ))"' | \
+  sed '0~10 s/$/\n\n---\n\nProduct Categories (continued...)\n/g'
+  )
+markdown_content+="\n\n---\n"
+
+echo -e "$markdown_content" >> "$markdown_file"
+
+# Data
+markdown_content=$(echo $data | \
+  sed -e 's/\\u003cp\\u003e//g; s/\\u003c\/p\\u003e//g; s/\\r//g; s/\\n//g; s/\/?did\\u003dap_card\\u0026trk\\u003dap_card//g; s/?did\\u003dap_card\\u0026trk\\u003dap_card//g; s/\\u0026nbsp\;//g;' | \
+  jq -r '.items[] | "<!-- header: \(.item.additionalFields.productCategory) -->", "## \(.item.additionalFields.productName)","\(.item.additionalFields.productSummary)", "\(.item.additionalFields.productUrl)","\n_Launched \(.item.additionalFields.launchDate)_", "\n---" ' | \
+  sed '$d')
+
+echo -e "$markdown_content" >> "$markdown_file"
+
+echo "Markdown file '$markdown_file' has been created."
